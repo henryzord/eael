3112b13007bc76595fec32599abe9d940f77c51a
henryzord <henry.cagnini@gmail.com>
2022-11-14 14:28:37 -0300

    now rendering authors names

--- scripts/master_table.py
+++ scripts/master_table.py
@@ -2,7 +2,38 @@ import json
 import pandas as pd
 import bibtexparser
 from bibtexparser.bibdatabase import BibDatabase
-from flask import render_template
+from pylatexenc.latex2text import LatexNodes2Text
+
+
+def render_author_names(string):
+    text = render_bibtex(string)
+    molecules = text.split(' and ')
+    rendered = []
+    et_al = False
+    for particle in molecules:
+        particle = particle.strip()
+        if particle == 'others':
+            rendered += ['et al.']
+            et_al = True
+            break
+
+        this_author = []
+        atoms = particle.split(',')
+        last_name = atoms[0].strip()
+        other_names = atoms[-1].split(' ')
+        for name in other_names:
+            if len(name) > 0:
+                this_author += [name.strip()[0]]
+        this_author += [last_name]
+        rendered += [' '.join(this_author)]
+
+    if not et_al:
+        return ', '.join(rendered)
+    return ' '.join(rendered)
+
+def render_bibtex(string):
+    text = LatexNodes2Text().latex_to_text(string)
+    return text
 
 
 def build_dataframe(d: dict) -> pd.DataFrame:
@@ -42,16 +73,15 @@ def build_html_table(df: pd.DataFrame, refs: BibDatabase) -> str:
     string = '''<table id="master_table">\n'''
 
     string += '\t<tr>\n'
-    string += '\t\t<th onclick="sortTable(0)">Authors</th>\n\t\t<th onclick="sortTable(1)">Title</th>\n\t\t<th onclick="sortTable(2)">Year</th>\n'
+    string += '\t\t<th onclick="sortTable(0)">Authors</th>\n\t\t<th onclick="sortTable(1)">Title</th>\n'
     for i, column in enumerate(df.columns):
-        string += '\t\t<th onclick="sortTable({0})">'.format(i + 3) + column + '</th>\n'
+        string += '\t\t<th onclick="sortTable({0})">'.format(i + 2) + column + '</th>\n'
     string += '\t</tr>\n'
 
     for i, row in df.iterrows():
         string += '\t<tr>\n'
-        string += '\t\t<td>' + refs.entries_dict[row.name]['author'] + '</td>\n'
-        string += '\t\t<td>' + refs.entries_dict[row.name]['title'][1:-1] + '</td>\n'
-        string += '\t\t<td>' + refs.entries_dict[row.name]['year'] + '</td>\n'
+        string += '\t\t<td>' + render_author_names(refs.entries_dict[row.name]['author']) + '</td>\n'
+        string += '\t\t<td>' + render_bibtex(refs.entries_dict[row.name]['title'][1:-1]) + '</td>\n'
         string += '\n'.join(['\t\t<td>' + str(x) + '</td>' for x in row])
         string += '\t</tr>\n'
 
@@ -64,7 +94,7 @@ def main(data_path, bibliography_path, template_path, write_path):
             open(data_path, 'r') as references_file, \
             open(bibliography_path, 'r') as refs_file, \
             open(template_path, 'r') as template_file, \
-            open(write_path, 'w') as write_file:
+            open(write_path, 'w', encoding='utf-8') as write_file:
         data = json.load(references_file)  # type: dict
         df = build_dataframe(data)
 
